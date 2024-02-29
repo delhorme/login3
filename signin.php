b910ff58c8a333ca2b11f28e502e55c29e55a808
Oxyridium <delhormelaurent@yahoo.fr>
2024-02-29 14:27:27 +0100

    ok

--- signin.php
+++ signin.php
@@ -2,29 +2,29 @@
 <html>
 
 <head>
-<title>Test - Login</title>
+	<title>Test - Login</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico" />
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
-<!--===============================================================================================-->	
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
-<!--===============================================================================================-->	
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="css/util.css">
 	<link rel="stylesheet" type="text/css" href="css/main.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 </head>
 
 <body>
@@ -43,15 +43,17 @@
 						<span class="focus-input100"></span>
 					</div>
 
-					<div class="wrap-input100 validate-input m-b-16" data-validate = "Please enter password">
+					<div class="wrap-input100 validate-input m-b-16" data-validate="Please enter password">
 						<!-- <input class="input100" type="password" name="pass" placeholder="Mot de passe"> -->
-						<input class="input100" type="password" name="password" id="password" placeholder="Entrez votre mot de passe" required>
+						<input class="input100" type="password" name="password" id="password"
+							placeholder="Entrez votre mot de passe" required>
 						<span class="focus-input100"></span>
 					</div>
 
-					<div class="wrap-input100 validate-input m-b-16" data-validate = "Please enter password">
+					<div class="wrap-input100 validate-input m-b-16" data-validate="Please enter password">
 						<!-- <input class="input100" type="password" name="pass" placeholder="Mot de passe"> -->
-						<input class="input100" type="password" name="cpassword" id="cpassword" placeholder="Confirmez votre mot de passe" required>
+						<input class="input100" type="password" name="cpassword" id="cpassword"
+							placeholder="Confirmez votre mot de passe" required>
 						<span class="focus-input100"></span>
 					</div>
 
@@ -68,62 +70,62 @@
 							Retour à la page de connexion
 						</a>
 					</div>
-					</div>
-				</form>
 			</div>
+			</form>
 		</div>
 	</div>
-<!--===============================================================================================-->
-<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
-<!--===============================================================================================-->
+	</div>
+	<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+	<!--===============================================================================================-->
 	<script src="vendor/animsition/js/animsition.min.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="vendor/bootstrap/js/popper.js"></script>
 	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="vendor/select2/select2.min.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="vendor/daterangepicker/moment.min.js"></script>
 	<script src="vendor/daterangepicker/daterangepicker.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="vendor/countdowntime/countdowntime.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="js/main.js"></script>
 
-    <?php
+	<?php
 
-    if (isset($_POST['formsend'])) {
+	if (isset($_POST['formsend'])) {
 
-        extract($_POST);
+		extract($_POST);
 
-        if (!empty($password) && !empty($cpassword) && !empty($email)) {
+		if (!empty($password) && !empty($cpassword) && !empty($email)) {
 
-            if ($password == $cpassword) {
-                $options = [
-                    'cost' => 12,
-                ];
+			if ($password == $cpassword) {
+				$options = [
+					'cost' => 12,
+				];
 
-                $hashpass = password_hash($password, PASSWORD_BCRYPT, $options);
+				$hashpass = password_hash($password, PASSWORD_BCRYPT, $options);
 
-                include 'includes/database.php';
-                global $db;
+				include 'includes/database.php';
+				global $db;
 
-                $q=$db->prepare("INSERT INTO users (email,password) VALUES(:email,:password)");
-                $q->execute([
-                    'email'=> $email,
-                    'password'  => $hashpass
-                
-                ]);
+				$q = $db->prepare("INSERT INTO users (email,password) VALUES(:email,:password)");
+				$q->execute([
+					'email' => $email,
+					'password' => $hashpass
+
+				]);
 
 
-            }
+			}
 
-        } else {
-            echo "les champs ne sont pas tous remplis";
-        }
-    }
+		} else {
+			echo "les champs ne sont pas tous remplis";
+		}
+	}
 
-    ?>
+	?>
 
 </body>
 
