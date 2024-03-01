9ccb78410ab727e1858b717637bff72d4385c893
Oxyridium <delhormelaurent@yahoo.fr>
2024-03-01 10:07:16 +0100

    ok

--- includes/database.php
+++ includes/database.php
@@ -2,12 +2,12 @@
 define('HOST', 'localhost');
 define('DB_NAME', 'u789471193_site');
 define('USER', 'u789471193_admin');
-define('PASS', '12756428Ld38!');
+define('PASS', '12756428Ld!');
 
 try {
     $db = new PDO("mysql:host=" . HOST . ";dbname=" . DB_NAME, USER, PASS);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "connection OK !";
+    echo "connection OK !";
 
 } catch (PDOException $e) {
     echo $e;
