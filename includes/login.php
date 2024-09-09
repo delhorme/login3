2fae608de7937ed610611b14b0d6d70f7f1771d2
Oxyridium <delhormelaurent@yahoo.fr>
2024-09-09 20:42:40 +0200

    a

--- includes/login.php
+++ includes/login.php
@@ -39,29 +39,3 @@ if (isset($_POST['formlogin'])) {
 }
 ?>
 
-    if (!empty($lemail) && !empty($lpassword)) {
-
-        $q = $db->prepare("SELECT * FROM users WHERE email = :email");
-        $q->execute(['email' => $lemail]);
-        $result = $q->fetch();
-
-        if ($result == TRUE) {
-            //le compte existe bien
-
-            if (password_verify($lpassword, $result['password'])) {
-                echo "Le mot de passe est bon, connexion en cours";
-                $_SESSION['email'] = $result['email'];
-                $_SESSION['password'] = $result['password'];
-
-            } else {
-                echo "Le mot de passe n'est pas correct";
-            }
-        } else {
-            echo "Le compte portant l'email " . $lemail . " n'existe pas";
-        }
-    } else {
-        echo "Veuillez compléter l'ensemble des champs";
-    }
-}
-
-
