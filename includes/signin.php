b632d4f7cead5037a3e6f4e57bccd430b2a7aaea
Oxyridium <delhormelaurent@yahoo.fr>
2024-10-04 10:34:57 +0200

    a

--- includes/signin.php
+++ includes/signin.php
@@ -41,5 +41,14 @@ exit;
 
     }
 }
-
+try {
+    $q = $db->prepare("INSERT INTO users (email, password) VALUES(:email, :password)");
+    $q->execute([
+        'email' => $email,
+        'password' => $hashpass
+    ]);
+    echo "Utilisateur créé avec succès !";
+} catch (PDOException $e) {
+    echo "Erreur lors de l'insertion : " . $e->getMessage();
+}
 ?>
