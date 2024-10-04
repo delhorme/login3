7029f7aa03a6d8be95f11bee255c2aef5c8a1f0a
Oxyridium <delhormelaurent@yahoo.fr>
2024-10-04 10:27:25 +0200

    a

--- includes/database.php
+++ includes/database.php
@@ -5,7 +5,7 @@ define('USER', 'u789471193_admin');
 define('PASS', '12756428Ld38!');
 
 try {
-    $db = new PDO('mysql:host=localhost;dbname=your_database', 'username', 'password');
+    $db = new PDO('mysql:host=localhost;dbname=your_database', 'email', 'password');
 
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
